07f2941353867647ec99ec8f05ad3b42a491fe9c
Akshay <akshayhuliyaru2005@gmail.com>
2025-09-04 01:45:07 +0530

    Firebase authentication

---
+++ client/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPoBKa8BBPLaonDzUx_ZEVtUPQBnVT3vQ",
+  authDomain: "zeroops-8a74e.firebaseapp.com",
+  projectId: "zeroops-8a74e",
+  storageBucket: "zeroops-8a74e.firebasestorage.app",
+  messagingSenderId: "1094869063552",
+  appId: "1:1094869063552:web:4d3586b8b1724eb9925365"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
